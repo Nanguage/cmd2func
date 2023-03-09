49e14c2fa51fe37675d311e56147944e578cfa70
Nanguage <Nanguage@yahoo.com>
2023-03-09 16:52:34 +0800

    fix for flake8

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ from setuptools import setup, find_packages
 def get_long_description():
     return "See https://github.com/Nanguage/cmd2func"
 
+
 def get_version():
     with open("cmd2func/__init__.py") as f:
         for line in f.readlines():
